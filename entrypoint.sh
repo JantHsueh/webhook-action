5ba840722678e46817c6588e48aea737cd641bcb
薛瑄 <384324069@qq.com>
2019-12-14 16:52:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
+curl -X POST -H "Content-Type: application/json" --data "{"msgtype": "text", "text": {"content": "github app 更新 内部版本号 $VERSION_CODE" 版本号 $VERSION_NAME"}}" $WEBHOOK_URL
