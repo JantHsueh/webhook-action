e46e4e620546cdb652b2ab9b8d14a948954ea442
薛瑄 <384324069@qq.com>
2019-12-14 17:30:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 
+chmod +x gradlew 
+./gradlew assembleRelease
 curl -X POST -H "Content-Type: application/json" --data "{"msgtype": "text", "text": {"content": "github app 更新 内部版本号 $VERSION_CODE 版本号 $VERSION_NAME"}}" $WEBHOOK_URL
