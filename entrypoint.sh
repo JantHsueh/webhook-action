1c285a3090c839e91289db48770f5a9ac70b52e5
xuexuan <384324069@qq.com>
2019-12-20 14:29:36 +0800

    增加输入tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-log=$(git log last_release.. --pretty=format:'%an, %cd %s \\n' --date=format:'%Y-%m-%d %H:%M:%S')
+echo "tag is $1"
+log=$(git log $1.. --pretty=format:'%an, %cd %s \\n' --date=format:'%Y-%m-%d %H:%M:%S')
 
 echo ::set-output name=log::$log
